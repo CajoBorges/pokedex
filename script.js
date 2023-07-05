5d601c16d29020c3c8957fe0bc7180db58c864d0
Carlos Borges <cjvborges@gmail.com>
2023-07-05 13:53:28 +0100

    Co-authored-by: gamertop13lol <gamertop13lol@users.noreply.github.com> Co-authored-by: Mário Rodrigues <MarioRodrigues10@users.noreply.github.com> Co-authored-by: Afonso Pimenta <pimenta.afonsosilva@gmail.com> Co-authored-by: OPerfeito11 <OPerfeito11@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,78 +1,114 @@
-// function table() {
-//   // Declare variables
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("myInput");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("myTable");
-//   tr = table.getElementsByTagName("tr");
-//   // Loop through all table rows, and hide those who don't match the search query
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
-
-
-
-fetch('./MOCK_DATA.json')
+fetch('./pokeinfo.json')
   .then(response => response.json())
   .then(data => {
-    table = document.querySelector('#pokemon-table')
-    data.forEach(element => {
-      tr = document.createElement('tr')
-      
-      td = document.createElement('td')
-      td.textContent = element.number
-      td.classList.add('number')
-      tr.appendChild(td)
-      
-      td = document.createElement('td')
-      td.textContent = element.name
-      td.classList.add('name')
-      tr.appendChild(td)
-      
-      td = document.createElement('td')
-      td.textContent = element.type
-      td.classList.add('type')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.hp
-      td.classList.add('hp')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.attack
-      td.classList.add('attack')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.defense
-      td.classList.add('defense')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.spAttack
-      td.classList.add('sp-attack')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.spDefense
-      td.classList.add('sp-defense')
-      tr.appendChild(td)
-
-      td = document.createElement('td')
-      td.textContent = element.stats.speed
-      td.classList.add('speed')
-      tr.appendChild(td)
-
-      table.appendChild(tr)
-    })
+    renderTable(data)
   })
+
+function renderTable(data) {
+  table = document.querySelector('#pokemon-table')
+  data.forEach(element => {
+    tr = document.createElement('tr')
+    tr.classList.add('row')
+    
+    img = document.createElement('img')
+    img.src = element.imagem
+    img.classList.add('image')
+    td = document.createElement('td')
+    text = document.createElement('span')
+    text.textContent = element.number
+    td.classList.add('table-item')
+    td.classList.add('number')
+    td.appendChild(img)
+    td.appendChild(text)
+    tr.appendChild(td)
+    
+    td = document.createElement('td')
+    td.textContent = element.name
+    td.classList.add('table-item')
+    td.classList.add('name')
+    tr.appendChild(td)
+    
+    td = document.createElement('td')
+    //td.textContent = element.tipo1
+    img = document.createElement('img')
+    img.src = tipoImagem(element.tipo1)
+    td.classList.add('table-item')
+    td.classList.add('type')
+    td.appendChild(img)
+    if (element.tipo2) {
+      //td.textContent += ' / ' + element.tipo2
+      img = document.createElement('img')
+      img.src = tipoImagem(element.tipo2)
+      td.appendChild(img)
+    }
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.hp
+    td.classList.add('table-item')
+    td.classList.add('hp')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.atk
+    td.classList.add('table-item')
+    td.classList.add('attack')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.def
+    td.classList.add('table-item')
+    td.classList.add('defense')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.spatk
+    td.classList.add('table-item')
+    td.classList.add('sp-attack')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.spdef
+    td.classList.add('table-item')
+    td.classList.add('sp-defense')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.textContent = element.stats.speed
+    td.classList.add('table-item')
+    td.classList.add('speed')
+    tr.appendChild(td)
+
+    table.appendChild(tr)
+  })
+}
+
+function searchFilter(pokemoneName, data) {
+    const asArray = Object.entries(data)
+    const filtered = asArray.filter(pokemon => pokemon[1].name.toLowerCase().includes(pokemoneName.toLowerCase()))
+    return Object.fromEntries(filtered)
+}
+
+function tipoImagem(tipo){
+    if (tipo == "Rock") return "./ima"
+    if (tipo == "Poison") return
+    if (tipo == "Flying") return
+    if (tipo == "Ground") return
+    if (tipo == "Fire") return
+    if (tipo == "Grass") return
+    if (tipo == "Water") return
+    if (tipo == "Fighting") return
+    if (tipo == "Bug") return
+    if (tipo == "Dark") return
+    if (tipo == "Psychic") return
+    if (tipo == "Ghost") return
+    if (tipo == "Dragon") return
+    if (tipo == "Electric") return
+    if (tipo == "Normal") return
+    if (tipo == "Steel") return
+    if (tipo == "Ice") return
+    if (tipo == "Fairy") return
+
+}
+
+
